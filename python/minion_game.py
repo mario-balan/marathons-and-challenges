d06548319256c6e0b0c4c1ba56f81433aec30918
Mario Balan <mario.balan@gmail.com>
2023-09-09 14:06:11 -0400

    Add hackerrank:minion_game.py

---
+++ python/minion_game.py
@@ -0,0 +1,20 @@
+def minion_game(s):
+    vowels='AEIOU'
+    kevin_score = 0
+    stuart_score = 0
+    for i in range(len(s)):
+        if s[i] not in vowels:
+            stuart_score += (len(s) - i)
+        else:
+            kevin_score += (len(s) - i)
+    if (stuart_score > kevin_score):
+        print('Stuart', stuart_score)
+    elif (kevin_score > stuart_score):
+        print('Kevin', kevin_score)
+    else:
+        print('Draw')
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
